385e1258309838e0af536742f31d7d2a7918a6f9
Mario Alejandro Gonzalez Cardona <alejandrogonzalez@uadec.edu.mx>
2018-04-15 13:32:04 -0500

    Repo Inicial

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=librarybooks-8ee7-Web-Deploy.pubxml -p:Password=pueecvK3kEPx6NzWukzSFew0vewC4LYDfMfsd0oPmd2GhzLWrSlCyhyzCg95
+
